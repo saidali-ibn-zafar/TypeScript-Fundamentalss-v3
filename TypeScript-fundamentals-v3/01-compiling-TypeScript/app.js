a6fe3d6338467faad430de2bd3443062cef0e1ec
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-07-07 17:31:35 +0200

    Add files via upload

---
+++ TypeScript-fundamentals-v3/01-compiling-TypeScript/app.js
@@ -0,0 +1,4 @@
+var character = "just typescript";
+console.log(character);
+// to compile write "tsc app.ts" or "tsc app.ts appjs"
+// if we do not write app.js like above, it will automatically create js file and translate i our typescript codes into javascript ...
---
+++ TypeScript-fundamentals-v3/01-compiling-TypeScript/app.ts
@@ -0,0 +1,6 @@
+const character = "just typescript";
+
+console.log(character);
+
+// to compile write "tsc app.ts" or "tsc app.ts appjs" on the terminal after installing typescript and more...
+// if we do not write app.js like above, it will automatically create js file and translate i our typescript codes into javascript ...
